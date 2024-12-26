494da96da1040e2781357c3ae3992ee572766fb6
SurendraRavikumar <surendraravikumar2001@gmail.com>
2024-12-26 23:40:53 +0530

    updated Once Again

--- routes/listing.js
+++ routes/listing.js
@@ -9,7 +9,7 @@ const {storage} = require("../cloudConfig.js");
 const upload = multer({ storage })
 
 // Index Route..........................
-// router.get("/", wrapAsync(listingController.index));
+router.get("/", wrapAsync(listingController.index));
 
 //Search Router.............................
 router.get("/search", isLoggedIn, wrapAsync(listingController.search));
@@ -18,7 +18,7 @@ router.get("/search", isLoggedIn, wrapAsync(listingController.search));
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 // Create Route...........................
-// router.post("/", isLoggedIn, upload.single("listing[image]"), validateListing, wrapAsync(listingController.createListing));
+router.post("/", isLoggedIn, upload.single("listing[image]"), validateListing, wrapAsync(listingController.createListing));
 
 // Show Route..............................
 router.get("/:id", wrapAsync(listingController.showListing));
